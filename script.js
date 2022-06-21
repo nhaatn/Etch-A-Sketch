77e40d87bbec4c49332dc0c799ac7bb334ace48d
Nhat <nhaatn1@gmail.com>
2022-06-21 13:44:17 -0600

    added an eventListener so t hat the cell can change to black on hover

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
+    // console.log(container);
+    // console.log(rows);
+    // console.log(cols);
+
     for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
-        cell.innerText = (i + 1);
+        //cell.innerText = (i + 1);   // Label the cell by numbers || DELETE LATER
         container.appendChild(cell).className = 'grid-item';
+
+        // When hovering on a "cell" the cell will turn black
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+            console.log(cell);
+        })
     };
 };
 
 makeRows(16,16);
-
